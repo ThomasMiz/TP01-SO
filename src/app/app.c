ce35b6eaf1ab04c8a8e1f5d8f85f5502435b4366
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2022-04-10 19:22:15 -0300

    Amount of workers chosen based on system info

--- src/app/app.c
+++ src/app/app.c
@@ -1,9 +1,59 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "./../shared/constants.h"
 #include "workerManagerADT.h"
 
-void onWorkerResult(workerManagerADT sender, unsigned int workerId, const TWorkerResult* result, void* arg);
-void onWorkerClosed(workerManagerADT sender, unsigned int workerId, void* arg);
+
+/** Callback function for whenever a worker returns a result. */
+static void onWorkerResult(workerManagerADT sender, unsigned int workerId, const TWorkerResult* result, void* arg);
+
+/** Callack function for whenever a worker closes.*/
+static void onWorkerClosed(workerManagerADT sender, unsigned int workerId, void* arg);
+
+
+/** Stores context information about the app process. */
+typedef struct {
+	/** An array with the files to process, of length fileCount. */
+	const const char** files;
+	
+	/** The total amount of files the app process was given. */
+	unsigned int fileCount;
+	
+	/** The amount of files that have already been sent to workers,
+	 * but not necessarily gotten results for.
+	 */
+	unsigned int filesSent;
+	
+	/** The amount of results received from workers. */
+	unsigned int resultsReceived;
+} TAppContext;
+
+
+/**
+ * Decides the amount of workers to spawn based on constants and
+ * the configuration of the running system.
+ *
+ * Will always return a value of at least 1, unless fileCount is 0.
+ */
+static unsigned int decideWorkerCount(unsigned int fileCount) {
+	long systemProcessors = sysconf(_SC_NPROCESSORS_ONLN);
+	
+#if LEAVE_ONE_PROCESSOR == 1
+	unsigned int workerLimit = systemProcessors <= 1 ? 1 : (systemProcessors - 1);
+#else
+	unsigned int workerLimit = systemProcessors <= 1 ? 1 : systemProcessors;
+#endif
+
+	if (fileCount > workerLimit)
+		fileCount = workerLimit;
+	
+#if MAX_WORKERS > 0
+	if (fileCount > MAX_WORKERS)
+		fileCount = MAX_WORKERS;
+#endif
+	
+	return fileCount;
+}
 
 int main(int argc, const char* argv[]) {
 	if (argc <= 1) {
@@ -11,32 +61,42 @@ int main(int argc, const char* argv[]) {
 		return EXIT_CODE_NOT_ENOUGH_PARAMS;
 	}
 	
-	// The amount of workers to spawn will be the amount of files
-	// we have to process, clamped to the maximum allowed.
-	unsigned int workerCount = argc - 1;
-	if (workerCount > MAX_WORKERS)
-		workerCount = MAX_WORKERS;
+	// We create a context struct and fill it up with initial data.
+	TAppContext appContext;
+	appContext.files = &argv[1];
+	appContext.fileCount = argc - 1;
+	appContext.filesSent = 0;
+	appContext.resultsReceived = 0;
 	
-	workerManagerADT workerManager = newWorkerManager(workerCount);
-	setWorkerResultCallback(workerManager, &onWorkerResult, NULL);
-	setWorkerClosedCallback(workerManager, &onWorkerClosed, NULL);
+	// Create a new workerManager with the desired amount of workers.
+	workerManagerADT workerManager = newWorkerManager(decideWorkerCount(appContext.fileCount));
+	fprintf(stderr, "Number of workers: %u\n", getWorkerCount(workerManager));// TODO: remove debug print
+	
+	// Set the callbacks on the workerManager. The parameter for the
+	// callbacks is a pointer to our app context struct. This is memory
+	// safe even though appContext is on the stack because the lifetime
+	// of the workerManager is entirely within this function, so the
+	// TAppContext is guaranteed to still be on the stack.
+	setWorkerResultCallback(workerManager, &onWorkerResult, &appContext);
+	setWorkerClosedCallback(workerManager, &onWorkerClosed, &appContext);
 	
 	for (int i=1; i<argc; i++)
-		sendWorkerTask(workerManager, (i - 1) % workerCount, i - 1, argv[i]);
+		sendWorkerTask(workerManager, (i - 1) % getWorkerCount(workerManager), i - 1, argv[i]);
 	
 	closeRemainingWorkers(workerManager);
 	pollUntilFinished(workerManager);
 	
+	// We free up all resources used by the workerManager.
 	freeWorkerManager(workerManager);
 	
-	fprintf(stderr, "[Master] Master process closing.\n");
+	fprintf(stderr, "[Master] Master process closing.\n"); // TODO: remove debug prints
 	return 0;
 }
 
-void onWorkerResult(workerManagerADT sender, unsigned int workerId, const TWorkerResult* result, void* arg) {
+static void onWorkerResult(workerManagerADT sender, unsigned int workerId, const TWorkerResult* result, void* arg) {
 	fprintf(stderr, "[Master] Worker %u returned result: taskId=%u, cantidadClausulas=%u, cantidadVariables=%u.\n", workerId, result->taskId, result->cantidadClausulas, result->cantidadVariables);
 }
 
-void onWorkerClosed(workerManagerADT sender, unsigned int workerId, void* arg) {
+static void onWorkerClosed(workerManagerADT sender, unsigned int workerId, void* arg) {
 	fprintf(stderr, "[Master] Worker %u closed.\n", workerId);
 }
--- src/shared/constants.h
+++ src/shared/constants.h
@@ -1,13 +1,32 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
+/** The name of the executable file containing the worker process. */
 #define WORKER_EXEC_FILE "worker.out"
+
+/**
+ * A hard limit on the maximum amount of workers that can be spawned
+ * by an app process. Set to -1 to disable this limit (this is ok
+ * because the app process will use the amount of CPUs on the system
+ * as a limit anyways).
+*/
+#define MAX_WORKERS -1
+
+/**
+ * Define this to 1 to ensure the app process will spawn less than
+ * one worker per system processor. Otherwise, it may spawn one
+ * worker per system processor.
+ *
+ * Spawning one worker per system processor might mean that work
+ * can be done faster, but on some operating systems can impact
+ * system responsiveness, specially if other processes are
+ * running at the same time. 
+ */
+#define LEAVE_ONE_PROCESSOR 1
 
 #define EXIT_CODE_NOT_ENOUGH_PARAMS 1
 #define EXIT_CODE_CREATE_PIPE_FAILED 2
 #define EXIT_CODE_FORK_FAILED 3
 #define EXIT_CODE_NOT_ENOUGH_MEMORY 4
-
-#define MAX_WORKERS 6
 
 #endif
